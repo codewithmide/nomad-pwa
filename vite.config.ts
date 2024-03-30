0d1fc71ce82f66e5531845935a229d0b32349e32
Olumide Micheal <olumidemicheal7@gmail.com>
2024-03-30 20:16:35 +0100

    final fix pwa error

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),  VitePWA({
-    injectRegister: 'auto',
-    manifest: {
-      name: 'Nomad',
-      short_name: 'Nomad',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#026CDF',
-      lang: 'en',
-      scope: '/',
-      icons: [
-        {
-          src: './pwaIcons/android-chrome-192x192.png',
-          sizes:'192x192',
-          type:'image/png',
-          purpose:'favicon'
-        },
-        {
-          src:'./pwaIcons/android-chrome-512x512.png',
-          sizes:'512x512',
-          type:'image/png',
-          purpose:'any'
-        },
-        {
-          src: './pwaIcons/apple-touch-icon.png',
-          sizes:'180x180',
-          type:'image/png',
-          purpose:'apple touch icon',
-        },
-        {
-          src: './pwaIcons/maskable_icon.png',
-          sizes:'512x512',
-          type:'image/png',
-          purpose:'maskable',
-        },
-      ],
-      theme_color: '#ffffff',
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt' as "prompt" | "autoUpdate",
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    icons: [{
+      src: '/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'favicon'
     },
-  }),]
-})
+    {
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
+    }],
+    theme_color: '#181818',
+    background_color: '#e0cc3b',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  },
+};
+
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
