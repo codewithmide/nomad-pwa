4517b46ec964c14d07009484ea18a5288a785100
Olumide Micheal <olumidemicheal7@gmail.com>
2024-03-30 20:06:24 +0100

    fix pwa error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),  VitePWA({
     injectRegister: 'auto',
@@ -16,27 +17,28 @@ export default defineConfig({
       icons: [
         {
           src: './pwaIcons/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-          purpose: 'any'
+          sizes:'192x192',
+          type:'image/png',
+          purpose:'favicon'
         },
         {
           src:'./pwaIcons/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'
+          sizes:'512x512',
+          type:'image/png',
+          purpose:'any'
         },
         {
           src: './pwaIcons/apple-touch-icon.png',
-          sizes: '180x180',
-          type: 'image/png',
+          sizes:'180x180',
+          type:'image/png',
+          purpose:'apple touch icon',
         },
         {
           src: './pwaIcons/maskable_icon.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable',
-        }
+          sizes:'512x512',
+          type:'image/png',
+          purpose:'maskable',
+        },
       ],
       theme_color: '#ffffff',
     },
